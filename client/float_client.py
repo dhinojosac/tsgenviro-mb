2447c0038578f3e4ec5c8f4603fc6f1ff15a5aad
Diego Hinojosa <d.hinojosa.cordova@gmail.com>
2020-12-01 08:38:47 -0300

    Create function to write/read float32 in registers

--- client/float_client.py
+++ client/float_client.py
@@ -13,6 +13,20 @@ logging.basicConfig(format=FORMAT)
 log = logging.getLogger()
 log.setLevel(logging.INFO)
 
+def write32Float(client,address, value):
+    builder = BinaryPayloadBuilder()
+    builder.add_32bit_float(value)
+    payload = builder.to_registers()
+    payload = builder.build()
+    client.write_registers(address, payload, skip_encode=True, unit=1) # write registers
+
+def read32Float(client,addres):
+    result = client.read_holding_registers(address, 2,  unit=1)
+    decoder = BinaryPayloadDecoder.fromRegisters(result.registers)
+    decoded = decoder.decode_32bit_float()
+    return decoded
+
+
 ip_address = 'localhost'
 
 client = ModbusTcpClient(ip_address, port=5020)
@@ -21,7 +35,7 @@ if client.connect():    # connection is OK
     # Create builder
     builder = BinaryPayloadBuilder()
      
-    DataFloat = -22.34
+    DataFloat = -512.34
 
     #builder.add_16bit_float(DataFloat)
     #builder.add_16bit_float(DataFloat)
@@ -34,6 +48,7 @@ if client.connect():    # connection is OK
     print("Writing Registers")
     print("-" * 60)
     print(payload)
+    print(len(payload))
     print("\n")
 
     payload = builder.build()
@@ -58,4 +73,12 @@ if client.connect():    # connection is OK
     print("-" * 60)
     print("%s\t", hex(decoded) if isinstance(decoded, int) else decoded)
 
+    print("-" * 60)
+    print("Test Functions")
+    print("-" * 60)
+
+    write32Float(client, 0, -34.1)
+    print (read32Float(client, 0))
+
     client.close()
+
